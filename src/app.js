4d95b6505b3e38fe3a09949a9a9084dee6ceef37
MaxwellJalves <maxwell880@gmail.com>
2021-10-10 00:54:27 -0300

    adicionado a funcao do eventListaner o scroll mais a logica de identificar se o Scrol está próximo do final da pagina , caso o retorno seja positivo a funcao exiba o lodar será chamada e exibira a animação por 1 segundo e logo apos ira sumir

--- src/app.js
+++ src/app.js
@@ -1,18 +1,26 @@
 let quantidade = 5;
-const pagina_atual = 1;
-const containerPost = document.querySelector('#posts-container')
+const pagina_atual = 0;
+const containerPost = document.querySelector("#posts-container");
+const containerLoad = document.querySelector('.loader')
+
+const obtemPaginacao = (valor) => {
+  const novaPagina = valor + 1;
+  return novaPagina;
+};
 
 //aguarda essa função realizar a atividade solicitada e  enquanto ela não chegar continue aguardando  , até receber a promessa resolvida
 const obterPosts = async () => {
   const resposta = await fetch(
-    `https://jsonplaceholder.typicode.com/posts?_limit=${quantidade}?&_page=${pagina_atual}`
+    `https://jsonplaceholder.typicode.com/posts?_limit=${quantidade}?&_page=${obtemPaginacao()}`
   );
   return resposta.json();
 };
 
 const adicionarPosts = async () => {
   const posts = await obterPosts();
-  const modeloDePosts = posts.map(({id, title , body}) => `
+  const modeloDePosts = posts
+    .map(
+      ({ id, title, body }) => `
   <div class="post"> 
     <div class="number">${id} </div>
     <div class="post-info">
@@ -22,8 +30,31 @@ const adicionarPosts = async () => {
     <div class="codigo"> </div>  
   </div>
   `
-  ).join('________________________________________________________________________________________________________________');
+    )
+    .join(
+      "________________________________________________________________________________________________________________"
+    );
   return (containerPost.innerHTML += modeloDePosts);
 };
 
 adicionarPosts();
+const removerOLoad = ()=>{
+    setTimeout(()=>{
+        containerLoad.classList.remove('show')
+    }, 1000)
+}
+
+const exibaOLoad = () =>{
+     containerLoad.classList.add('show')
+     removerOLoad()
+}
+
+window.addEventListener("scroll", () => {
+  const { scrollTop, clientHeight, scrollHeight } = document.documentElement;
+  const proximoAoFinalDaPagina = scrollTop + clientHeight >= scrollHeight - 30;
+
+  if (proximoAoFinalDaPagina) {
+    exibaOLoad()
+
+  }
+});
